da76b639f3de4013451d40a23dd35a68785c2efa
AriyanDC <ariyan.vafaie@gmail.com>
2022-08-18 23:24:10 +0300

    Basic rect on mouseclick

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 public class Main {
+    static MyPanel p;
+    static MyFrame f;
     public static void main(String[] args) {
-        new MyFrame();
+        f = new MyFrame();
+        p = f.p;
+        f.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                p.xPositions.add(p.getMousePosition().x);
+                p.yPositions.add(p.getMousePosition().y);
+                p.widths.add(10);
+                p.heights.add(10);
+                p.repaint();
+                System.out.println(p.xPositions);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
 }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MyPanel extends JPanel {
+    public ArrayList<Integer> xPositions = new ArrayList<Integer>();
+    public ArrayList<Integer> yPositions = new ArrayList<Integer>();
+    public ArrayList<Integer> widths = new ArrayList<Integer>();
+    public ArrayList<Integer> heights = new ArrayList<Integer>();
     MyPanel() {
         this.setPreferredSize(new Dimension(500,500));
     }
 
     public void paint(Graphics g3D) {
         Graphics2D g = (Graphics2D) g3D;
-        g.drawLine(0,0,500,500);
+        for (int i = 0; i < xPositions.size(); i++) {
+            g.fillRect(xPositions.get(i),yPositions.get(i),widths.get(i),heights.get(i));
+        }
     }
+
 }
